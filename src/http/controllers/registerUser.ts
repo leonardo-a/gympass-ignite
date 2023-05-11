5cc6734ead8c766c638bf706ed1ba43f047dc6f0
Leonardo-a <leolealantao@gmail.com>
2023-05-10 23:23:17 -0300

    chore: create user password hash

---
+++ src/http/controllers/registerUser.ts
@@ -0,0 +1,37 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { hash } from 'bcryptjs'
+
+import { prisma } from '@/libs/prisma'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  const passwordHash = await hash(password, 6)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      passwordHash,
+    },
+  })
+
+  return reply.status(201).send()
+}
